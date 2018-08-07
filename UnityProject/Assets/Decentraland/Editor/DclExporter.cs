f7bca4f7ffa412adac7cafe5924d2a36b0ab00fb
Stephen Zhou <stv1024@gmail.com>
2018-08-07 22:35:20 +0800

    add .dcl node. add EditorWindow cs file

---
+++ UnityProject/Assets/Decentraland/Editor/DclExporter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class DclExporter : EditorWindow
+{
+    [MenuItem("Decentraland/Scene Exporter")]
+    static void Init()
+    {
+        var window = (DclExporter)GetWindow(typeof(DclExporter));
+        window.Show();
+    }
+    
+    void OnGUI()
+    {
+
+    }
+}
