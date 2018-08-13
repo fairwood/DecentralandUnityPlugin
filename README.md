899505d47f8589b135790870f15e649f500cf4f8
Stephen Zhou <stv1024@gmail.com>
2018-08-13 19:33:57 +0800

    sample_dcl project

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# DecentralandUnityPlugin
+# Decentraland Unity Plugin
+## Installation Guide for Artists
+1. Follow [DCL Installation Guide](https://docs.decentraland.org/documentation/installation-guide/)
+1. 
--- UnityProject/Assets/Decentraland/Editor/DclExporter.cs
+++ UnityProject/Assets/Decentraland/Editor/DclExporter.cs
@@ -11,9 +11,29 @@ public class DclExporter : EditorWindow
         var window = (DclExporter)GetWindow(typeof(DclExporter));
         window.Show();
     }
-    
+
     void OnGUI()
     {
+        if (GUILayout.Button("Hey")){
+            GetSceneFileTemplate();
+        }
+    }
 
+    string GetSceneFileTemplate()
+    {
+        var guids = AssetDatabase.FindAssets("dcl_scene_template");
+        if (guids.Length <= 0)
+        {
+            if (EditorUtility.DisplayDialog("Cannot find dcl_scene_template.txt in the project!", "Please re-install Decentraland Unity Plugin asset to fix this problem.", "Re-install", "Back"))
+            {
+                //TODO:
+            }
+            return null;
+        }
+        var path = AssetDatabase.GUIDToAssetPath(guids[0]);
+        Debug.Log("Path:" + path);
+        var template = AssetDatabase.LoadAssetAtPath(path, typeof(TextAsset)) as TextAsset;
+        Debug.Log("Txt:" + template.text);
+        return template.text;
     }
 }
