fe40c4639af5a0b165c48d39d7914a85edce575a
Stephen Zhou <stv1024@gmail.com>
2018-09-04 23:30:09 +0800

    precompile for cmd

--- UnityProject/Assets/Decentraland/Editor/DclCLI.cs
+++ UnityProject/Assets/Decentraland/Editor/DclCLI.cs
@@ -8,34 +8,42 @@ namespace Dcl
 {
     public class DclCLI
     {
-        public static void ExecuteCommand (string cmd)
+        public static void ExecuteCommand(string cmd)
         {
-            var thread = new Thread(delegate () {Command(cmd);});
+            var thread = new Thread(delegate() { Command(cmd); });
             thread.Start();
         }
- 
-        static void Command (string cmd)
+
+        static void Command(string cmd)
         {
             var processInfo = new ProcessStartInfo("cmd.exe", cmd);
             processInfo.CreateNoWindow = false;
             processInfo.UseShellExecute = false;
-    
+
             var process = Process.Start(processInfo);
-            
+
             process.WaitForExit();
             process.Close();
         }
-        
+
         public static void DclInit(string path)
         {
+#if UNITY_EDITOR_OSX
+
+#else
             var cmd = string.Format("/k cd /d {0} & dcl init", path);
             ExecuteCommand(cmd);
+#endif
         }
-        
+
         public static void DclStart(string path)
         {
+#if UNITY_EDITOR_OSX
+
+#else
             var cmd = string.Format("/k cd /d {0} & dcl start", path);
             ExecuteCommand(cmd);
+#endif
         }
     }
 }
