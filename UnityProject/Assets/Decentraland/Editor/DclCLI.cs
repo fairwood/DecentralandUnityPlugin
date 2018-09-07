b6e74c770923ac136506526eba82e28026354ac0
alking <aijingsun6@gmail.com>
2018-09-08 00:57:14 +0800

    run dcl start on macos

--- UnityProject/Assets/Decentraland/Editor/DclCLI.cs
+++ UnityProject/Assets/Decentraland/Editor/DclCLI.cs
@@ -16,12 +16,18 @@ namespace Dcl
 
         static void Command(string cmd)
         {
-            var processInfo = new ProcessStartInfo("cmd.exe", cmd);
+            UnityEngine.Debug.Log(String.Format("exec cmd {0}",cmd));
+            string exe = "";
+#if UNITY_EDITOR_OSX
+            exe = "/bin/bash";
+#else
+            exe = "cmd.exe";
+
+#endif
+            var processInfo = new ProcessStartInfo(exe, cmd);
             processInfo.CreateNoWindow = false;
             processInfo.UseShellExecute = false;
-
             var process = Process.Start(processInfo);
-
             process.WaitForExit();
             process.Close();
         }
@@ -29,7 +35,9 @@ namespace Dcl
         public static void DclInit(string path)
         {
 #if UNITY_EDITOR_OSX
-
+            string from = String.Format("{0}/Decentraland/template/*",Application.dataPath);
+            string cmd = String.Format("-c 'cd {0};source $HOME/.bash_profile; cp {1} {0};rm {0}/*.meta;npm install'",path,from);
+            ExecuteCommand(cmd);
 #else
             var cmd = string.Format("/k cd /d {0} & dcl init", path);
             ExecuteCommand(cmd);
@@ -39,7 +47,8 @@ namespace Dcl
         public static void DclStart(string path)
         {
 #if UNITY_EDITOR_OSX
-
+            string cmd = String.Format("-c 'cd {0};source $HOME/.bash_profile; dcl start'", path);
+            ExecuteCommand(cmd);
 #else
             var cmd = string.Format("/k cd /d {0} & dcl start", path);
             ExecuteCommand(cmd);
