54906319f16a91579184f47b4523c160ffe1fcef
mickychain <mickychain@sina.cn>
2018-09-17 18:41:07 +0800

    tree sample. Unity Tree can be exported!

--- dcl/scene.tsx
+++ dcl/scene.tsx
@@ -11,6 +11,7 @@ export default class UnityScene extends DCL.ScriptableScene<any, IState> {
     return (
       <scene position={{x:5,y:0,z:5}}>
         <entity position={{x:0,y:1,z:-10}} scale={{x:1,y:1,z:1}} rotation={{x:0,y:0,z:0}}></entity>
+        <gltf-model position={{x:0,y:0,z:0}} scale={{x:1,y:1,z:1}} rotation={{x:0,y:0,z:0}} src="./unity_assets/Tree.gltf"></gltf-model>
         <entity position={{x:0,y:3,z:0}} scale={{x:1,y:1,z:1}} rotation={{x:50,y:330,z:0}}></entity>
         <entity position={{x:0,y:0,z:0}} scale={{x:1,y:1,z:1}} rotation={{x:0,y:0,z:0}}></entity>
       </scene>
