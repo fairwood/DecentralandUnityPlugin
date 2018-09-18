4e9badb627811acc67e54c30601079c8006a2fa8
alking <aijingsun6@gmail.com>
2018-09-18 12:21:21 +0800

    modify scene

--- dcl/scene.tsx
+++ dcl/scene.tsx
@@ -11,9 +11,11 @@ export default class UnityScene extends DCL.ScriptableScene<any, IState> {
     return (
       <scene position={{x:5,y:0,z:5}}>
         <entity position={{x:0,y:1,z:-10}} scale={{x:1,y:1,z:1}} rotation={{x:0,y:0,z:0}}></entity>
-        <gltf-model position={{x:0,y:0,z:0}} scale={{x:1,y:1,z:1}} rotation={{x:0,y:0,z:0}} src="./unity_assets/Tree.gltf"></gltf-model>
         <entity position={{x:0,y:3,z:0}} scale={{x:1,y:1,z:1}} rotation={{x:50,y:330,z:0}}></entity>
         <entity position={{x:0,y:0,z:0}} scale={{x:1,y:1,z:1}} rotation={{x:0,y:0,z:0}}></entity>
+        <box position={{x:0,y:0,z:-0.12}} scale={{x:1,y:1,z:1}} rotation={{x:0,y:0,z:0}} material="#Board"></box>
+        <cone position={{x:1.015023,y:0.4215235,z:-1.400313}} scale={{x:1,y:1,z:1}} rotation={{x:49.90312,y:0,z:0}}></cone>
+        <material id="Board" albedoColor="#FFFFFF" albedoTexture="./unity_assets/Assets/Decentraland/Sample - 01/Materials/colormap.png" bumpTexture="./unity_assets/Assets/Decentraland/Sample - 01/Materials/bump.jpg" emissiveColor="#000000" metallic={0} roughness={0.794}/>
       </scene>
     )
   }
