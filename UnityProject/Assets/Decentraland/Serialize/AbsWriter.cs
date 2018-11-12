b57180a5371fdae5e6db8956958e476fb42f9833
alking <aijingsun6@gmail.com>
2018-11-12 13:20:41 +0800

    add writer class

---
+++ UnityProject/Assets/Decentraland/Serialize/AbsWriter.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+/// <summary>
+/// 抽象Writer
+/// 使用方法：
+/// 可以通过传入不同的的Stream，来实现文件写入，或者网络写入
+/// 如果内容除了Node信息之外，还有其他数据，可以覆写方法：PreWrite,PostWrite
+/// </summary>
+public  abstract class AbsWriter
+{
+	private StreamWriter stream;
+
+	public AbsWriter(Stream stream)
+	{
+		this.stream = new StreamWriter(stream, System.Text.Encoding.UTF8);
+	}
+
+	public void PreWrite(StreamWriter stream) { }
+
+	public void PostWrite(StreamWriter stream) { }
+
+	public void Write(WriteableNode node)
+	{
+		PreWrite(stream);
+		node.Write(stream);
+		PostWrite(stream);
+		stream.Flush();
+	}
+
+}
---
+++ UnityProject/Assets/Decentraland/Serialize/WriteableNode.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+/// <summary>
+/// 节点抽象类
+/// 写入顺序:
+/// 1.PreWrite
+/// 2.WriteChildren 
+///   2.1 BeforeChild
+///   2.2 Child.Write
+///   2.3 AfterChild
+/// 3.PostWrite
+/// </summary>
+public abstract class WriteableNode
+{
+	void PreWrite(StreamWriter stream) { }
+
+	void PostWrite(StreamWriter stream) { }
+
+	void BeforeChild(StreamWriter stream, WriteableNode child) { }
+
+	public abstract List<WriteableNode> Children();
+
+	void AfterChild(StreamWriter stream, WriteableNode child) { }
+
+	/// <summary>
+	/// 建造者模式
+	/// </summary>
+	/// <param name="stream"></param>
+	public void Write(StreamWriter stream)
+	{
+		PreWrite(stream);
+
+		List<WriteableNode> children = Children();
+
+		foreach (WriteableNode node in children)
+		{
+			BeforeChild(stream,node);
+
+			node.Write(stream);
+
+			AfterChild(stream,node);
+	
+		}
+		PostWrite(stream);
+	}
+}
